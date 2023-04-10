5df0462e0f06cf2c330dbab915adba3eacc5a026
Bence <manyokibenc@gmail.com>
2023-04-10 14:52:22 +0200

    Docker and deployment test

--- packages/client-web/postbuild.js
+++ packages/client-web/postbuild.js
@@ -11,6 +11,7 @@ const fileContent = readFileSync(serverJsFilePath, 'utf-8');
 // regexes
 const exportedEnvJSONRegex = new RegExp(`"env":{.*},"webpack"`);
 const serverPortConstRegex = new RegExp('const currentPort =.*');
+const serverHostConstRegex = new RegExp('const hostname =.*');
 
 // get env object
 const match = fileContent.match(exportedEnvJSONRegex).toString().replace(',"webpack"', '');
@@ -22,5 +23,8 @@ if (!envPort)
     throw new Error(`Env does not export a required value: PORT!`);
 
 // write file
-const replaced = fileContent.replace(serverPortConstRegex, `const currentPort = ${envPort}`)
+const replaced = fileContent
+    .replace(serverPortConstRegex, `const currentPort = ${envPort}`)
+    .replace(serverHostConstRegex, `const hostname = "localhost"`);
+
 writeFileSync(serverJsFilePath, replaced, 'utf-8');
