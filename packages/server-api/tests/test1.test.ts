3356fc60ebdedcbb7cf385577bec48679c2ce519
Bence <manyokibenc@gmail.com>
2023-04-11 19:13:24 +0200

    A test added using DI with mockable services.

--- packages/server-api/tests/test1.test.ts
+++ packages/server-api/tests/test1.test.ts
@@ -1,7 +1,13 @@
-import { instantiateSingletonServices } from '../src/instantiation'
-test('something', () => {
+import { instantiateSingletonServices, instantiateTransientServices } from '../src/instantiation'
+
+test('something', async () => {
 
     const singletons = instantiateSingletonServices();
+    const transientServices = instantiateTransientServices(singletons);
+
+    const movies = transientServices
+        .movieService
+        .getMoviesAsync('');
 
     expect(singletons.configService.trpcPort === 5002);
 });
